4b395acce32aa186f53dcb1dd973c12326c44bc6
Javier <javier.garcia1485@gmail.com>
2025-03-14 18:58:19 -0500

    Fixing Test Failures

--- src/main/java/demo/tests/DemoTest.java
+++ src/main/java/demo/tests/DemoTest.java
@@ -2,6 +2,8 @@ package demo.tests;
 
 import demo.base.BaseTest;
 import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -18,27 +20,25 @@ public class DemoTest extends BaseTest { // DemoTest class extends BaseTest to i
     @Description("Verify unsuccessful login with invalid credentials")
     public void testUnsuccessfulLogin() {
         loginPage.login("test_user", "test_pass"); // Enter invalid credentials and attempt login
-        Assert.assertFalse(productPage.isProductListDisplayed(), "Login should have failed but didn't"); // Verify login failure by checking if the product list is not displayed
+        String errorMessage = driver.findElement(By.cssSelector("div.error-message-container.error")).getText();
+        Assert.assertTrue(errorMessage.contains("Epic sadface"));// Verify the logIn failed message
     }
 
     @Test // Test case to verify that the product catalog is accessible
     @Description("Verify product catalog navigation and validation")
     public void testProductNavigation() {
+        testSuccessfulLogin();
         Assert.assertTrue(productPage.isProductListDisplayed(), "Product list is not displayed"); // Ensure the product list is visible
     }
 
-    @Test // Test case to add products to the shopping cart
-    @Description("Add three products to the cart")
-    public void testAddProductsToCart() {
+    @Test // Test case to add products to the shopping cart and to complete the checkout process
+    @Description("Complete the checkout process")
+    public void testCheckoutProcess() {
+        testSuccessfulLogin();
         for (int i = 0; i < 3; i++) { // Loop to add three products to the cart
             productPage.addProductToCart(); // Click Add to Cart button
         }
         cartPage.openCart(); // Open the shopping cart page
-    }
-
-    @Test // Test case to complete the checkout process
-    @Description("Complete the checkout process")
-    public void testCheckoutProcess() {
         checkoutPage.proceedToCheckout(); // Click the checkout button to proceed
         checkoutPage.fillCheckoutForm("Test", "QA", "12345"); // Fill in checkout details
         checkoutPage.finishCheckout(); // Complete the checkout and verify success
